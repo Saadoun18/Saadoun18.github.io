378ebb5fc12b9032420b712d504b786f3216302e
Saadoun18 <Saeed.Ghanem@proton.me>
2026-07-28 22:52:53 +0300

    Update(add): iframe for Resume

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,17 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+const showButton = document.getElementById('previewBtn');
+const closeButton = document.getElementById('closePreviewBtn');
+    const iframeWrapper = document.getElementById('iframeWrapper');
+
+// When "Show Iframe" is clicked, change display to block
+showButton.addEventListener('click', function() {
+    iframeWrapper.style.display = 'block';
+});
+
+// When "X" is clicked, change display back to none
+closeButton.addEventListener('click', function() {
+    iframeWrapper.style.display = 'none';
+});
